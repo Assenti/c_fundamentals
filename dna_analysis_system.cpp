836f4395a3cfaa9b0068f7f282458c47d0391d8d
Asset Sultanov <34131701+Assenti@users.noreply.github.com>
2018-03-15 21:47:43 +0600

    Update dna_analysis_system.cpp

--- dna_analysis_system.cpp
+++ dna_analysis_system.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 /**************************Global Variable Definition***************************/
 const int total_patients_number = 7;
-const int dna_size = 1000; //1000 - for good PC performance
+const int dna_size = 10000; //1000 - for good PC performance
 const int doctors_number = 3;
 
 /*******************************Structure Definition*********************************/
@@ -24,10 +24,11 @@ struct Patient {
 	string patient_id;
 	int blood_group;
 	int age;
-	char dna_sequence[dna_size]; 
+	char dna_sequence[dna_size];
 };
 
-struct PatientDnaPercentage {
+struct PatientDnaPercentage 
+{
 	string patient_id;
 	double dna_analysis_result[4];
 };
@@ -63,16 +64,16 @@ Doctor doctor_records[doctors_number] =
 
 Patient patient_records[total_patients_number] =
 {
-	{ "Hugh", "S12", 2 , 50},
-	{ "Mark", "C13", 1 , 40},
-	{ "Kate", "A31", 3 , 60},
-	{ "Brad", "B12", 2 , 50},
-	{ "Ryan", "A23", 2 , 35},
-	{ "Alen", "GG2", 2 , 70},
-	{ "John", "AID", 2 , 70}
+	{ "Hugh", "S12", 2 , 50 },
+	{ "Mark", "C13", 1 , 40 },
+	{ "Kate", "A31", 3 , 60 },
+	{ "Brad", "B12", 2 , 50 },
+	{ "Ryan", "A23", 2 , 35 },
+	{ "Alen", "GG2", 2 , 70 },
+	{ "John", "AID", 2 , 70 }
 };
 
-PatientDnaPercentage dna_results[total_patients_number*3] =
+PatientDnaPercentage dna_results[total_patients_number * 3] =
 {
 	{ "S12" },
 	{ "C13" },
@@ -83,7 +84,7 @@ PatientDnaPercentage dna_results[total_patients_number*3] =
 	{ "AID" }
 };
 
-PatientDnaElements dna_elems_compare_results[total_patients_number*3] =
+PatientDnaElements dna_elems_compare_results[total_patients_number * 3] =
 {
 	{ "S12" },
 	{ "C13" },
@@ -125,9 +126,11 @@ bool DnaDifferenceResultByDnaElems_sort(
 
 void find_percentage_similarity();
 
-int levenstein(char* value1, char*  value2);
+int levenstein(string value1, string  value2);
 
 void patients_with_max_compatibilty_by_DNA_elems();
+
+int find_composition_similarity(string first_patiend_id, string second_patient_id);
 
 void artificial_dna_tests(double Adenin_percent, double Guanin_percent,
 	double Citosin_percent, double Timin_percent);
@@ -225,22 +228,22 @@ void find_percentage_similarity() {
 	cout << "\n----Find patients with the best similarity to each other by percentage ratio----" << endl;
 	display_patients_list();
 	//int current_patient_index = get_result_index_by_id(id);
-	DnaDifferenceResultByPercentCompare results[total_patients_number*3];
+	DnaDifferenceResultByPercentCompare results[total_patients_number * 3];
 	int results_iterator = 0;
 	int size = total_patients_number - 1;
 	double total_difference = 0;
 	int i = 0, j = 0, k = 0;
 	int min = INT_MAX;
 	int best_index = 0;
-	
+
 	for (i = 0; i <= total_patients_number; i++)
 	{
 		for (j = i + 1; j < total_patients_number; j++)
 		{
-			for (k = 0; k < 4; k++) 
-				{
-					total_difference += abs(dna_results[i].dna_analysis_result[k] -	dna_results[j].dna_analysis_result[k]);
-				}
+			for (k = 0; k < 4; k++)
+			{
+				total_difference += abs(dna_results[i].dna_analysis_result[k] - dna_results[j].dna_analysis_result[k]);
+			}
 			results[results_iterator] = { dna_results[i].patient_id, dna_results[j].patient_id, total_difference };
 
 			//cout << dna_results[i].patient_id << " and " << dna_results[j].patient_id << " : " << total_difference << endl;
@@ -249,7 +252,7 @@ void find_percentage_similarity() {
 		}
 	}
 
-	sort(results, results + (total_patients_number*3), DnaDifferenceResultByPercent_sort);
+	sort(results, results + (total_patients_number * 3), DnaDifferenceResultByPercent_sort);
 
 	cout << "Percentage ratio of patients DNA:" << endl;
 
@@ -263,7 +266,7 @@ void find_percentage_similarity() {
 		cout << results[i].patient_a_id << " and " << results[i].patient_b_id <<
 			". Difference value: " << results[i].percentage_difference_result << endl;
 	}
-	
+
 	for (int i = 0; i < total_patients_number * 3; i++)
 	{
 		if ((int)results[i].percentage_difference_result == min)
@@ -326,13 +329,13 @@ void display_patients_list()
 				break;
 			}
 		}
-		cout << endl;	
+		cout << endl;
 	}
 }
 
-int levenstein(char* value1, char*  value2) {
-	int height = strlen(value1) + 1;
-	int length = strlen(value2) + 1;
+int levenstein(string value1, string value2) {
+	int height = value1.length() + 1;
+	int length = value2.length() + 1;
 	int** matrix = new int*[height];
 	for (int i = 0; i < height; i++) {
 		matrix[i] = new int[length];
@@ -362,42 +365,76 @@ int levenstein(char* value1, char*  value2) {
 	return matrix[height - 1][length - 1];
 }
 
+char* get_dna_sequence_by_patient_id(string patient_id) {
+	for (int i = 0; i < total_patients_number; i++) {
+		if (patient_records[i].patient_id == patient_id) {
+			return patient_records[i].dna_sequence;
+		}
+	}
+}
+
+int find_composition_similarity(string first_patiend_id, string second_patient_id) {
+	char* first_dna_sequence =	get_dna_sequence_by_patient_id(first_patiend_id);
+	char* second_dna_sequence = get_dna_sequence_by_patient_id(second_patient_id);
+
+	int start_position = 0, offset_value = 10;
+	int total_composition_difference = 0;
+
+	while (start_position != 10000) {
+		int finish_value = start_position + offset_value;
+
+		string first_dna_chunk, second_dna_chunk;
+
+		for (int i = start_position; i < finish_value; i++) {
+			first_dna_chunk += first_dna_sequence[i];
+			second_dna_chunk += second_dna_sequence[i];
+		}
+
+		int difference = levenstein(
+			first_dna_chunk, second_dna_chunk);
+		total_composition_difference += difference;
+		start_position += offset_value;
+	}
+	return total_composition_difference;
+}
+
 void patients_with_max_compatibilty_by_DNA_elems()
 {
 	cout << "-----------Find patients with maximum compatibilty by comparing DNA elements-------------" << endl;
 	cout << "The analysis is in progress... ";
-	
+
 	int difference = 0;
 	int min = INT_MAX;
-	DnaDifferenceResultByElemsCompare dif_results[total_patients_number*3];
+	DnaDifferenceResultByElemsCompare dif_results[total_patients_number * 3];
 	int res_iterator = 0;
 
 	for (int i = 0; i <= total_patients_number; i++)
 	{
 		for (int j = i + 1; j < total_patients_number; j++)
 		{
-			difference = levenstein(patient_records[i].dna_sequence, patient_records[j].dna_sequence);
+			difference = find_composition_similarity(patient_records[i].patient_id, patient_records[j].patient_id);
+			
 			if (difference < min)
 			{
 				min = difference;
 			}
-			
-			dif_results[res_iterator] = { dna_elems_compare_results[i].patient_id,
-			dna_elems_compare_results[j].patient_id, difference };
 
-			/*cout << dna_elems_compare_results[i].patient_name << " and " << dna_elems_compare_results[j].patient_name << "\tDifference value: " <<
-				difference << endl;*/
+			dif_results[res_iterator] = { dna_elems_compare_results[i].patient_id,
+				dna_elems_compare_results[j].patient_id, difference };
+
+			cout << dna_elems_compare_results[i].patient_id << " and " << dna_elems_compare_results[j].patient_id << "\tDifference value: " <<
+			difference << endl;
 
 			res_iterator++;
 		}
 	}
 
-	sort(dif_results, dif_results + (total_patients_number*3), DnaDifferenceResultByDnaElems_sort);
+	sort(dif_results, dif_results + (total_patients_number * 3), DnaDifferenceResultByDnaElems_sort);
 	cout << "The analysis is completed." << endl;
 	cout << "Patients compatibility by DNA elements are: " << endl;
-	for (int i = 0; i < total_patients_number*3; i++)
+	for (int i = 0; i < total_patients_number * 3; i++)
 	{
-		cout << i+1 << ") " << dif_results[i].patient_a_id << " and " << dif_results[i].patient_b_id <<
+		cout << i + 1 << ") " << dif_results[i].patient_a_id << " and " << dif_results[i].patient_b_id <<
 			"\tDifference value: " << dif_results[i].dna_elements_difference_result << endl;
 
 	}
@@ -407,7 +444,7 @@ void patients_with_max_compatibilty_by_DNA_elems()
 		if (dif_results[i].dna_elements_difference_result == min)
 		{
 			cout << "Patients with the best ratio: " << dif_results[i].patient_a_id << " and "
-				<< dif_results[i].patient_b_id << ". Difference value: " 
+				<< dif_results[i].patient_b_id << ". Difference value: "
 				<< dif_results[i].dna_elements_difference_result << endl;
 		}
 	}
@@ -436,7 +473,7 @@ void artificial_dna_tests(double Adenin_percent, double Guanin_percent,
 		double G = (dna_size / 100) * Guanin_percent;
 		double C = (dna_size / 100) * Citosin_percent;
 		double T = (dna_size / 100) * Timin_percent;
-		if (random_num > 0 && random_num <= A)
+		if (random_num >= 0 && random_num <= A)
 		{
 			dna_sequence[i] = 'A';
 		}
@@ -456,19 +493,37 @@ void artificial_dna_tests(double Adenin_percent, double Guanin_percent,
 		}
 	}
 	cout << "Arttificial DNA sequence generated" << endl;
-	cout << "Do you want to display DNA sequence? (0 - no, yes - 1)" << endl;
+	cout << "Do you want to display DNA each elements amount? (0 - no, yes - 1)" << endl;
 	int doctors_choice;
 	cin >> doctors_choice;
-	
+
+	int A_counter = 0, G_counter = 0, C_counter = 0, T_counter = 0;
+
 	if (doctors_choice == 1)
 	{
 		for (int i = 0; i < dna_size; i++)
+		{
+			if (dna_sequence[i] == 'A')
 			{
-				cout << dna_sequence[i];
+				A_counter++;
 			}
+			else if (dna_sequence[i] == 'G')
+			{
+				G_counter++;
+			}
+			else if (dna_sequence[i] == 'C')
+			{
+				C_counter++;
+			}
+			else
+			{
+				T_counter++;
+			}
+		}
 	}
-	
-	cout << endl;
+
+	cout << A_counter << "(A) " << G_counter 
+		<< "(G) " << C_counter << "(C) " << T_counter << "(T)" << endl;
 }
 
 void artificial_dna_tests_modul()
@@ -544,8 +599,8 @@ void initialize_system() {
 		return;
 	}
 
-	
-	
+
+
 }
 
 void autentification(string name, string password)
@@ -571,14 +626,14 @@ void autentification(string name, string password)
 		{
 			initialize_system();
 		}
-		
+
 	}
 	else
 	{
 		cout << "Wrong name or password, please try again" << endl;
 		autentification_menu();
 	}
-	
+
 }
 
 void autentification_menu()
